b68dbdfc6bf0e431605bc908e59239db31a138ee
sleeping-anonymous <157833279+sleeping-anonymous@users.noreply.github.com>
2025-07-07 18:56:41 +0000

    Updated js to work with U.I. using dom

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let compChoice;
 let userScore = 0;
 let compScore = 0;
 
-function humanChoice() {
-    let choice = prompt("Enter your choice:").toLowerCase();
+function humanChoice(e) {
+    let choice = e.target.textContent.toLowerCase();
     return choice;
 }
 
@@ -16,49 +16,64 @@ function computerChoice() {
     return choice;
 }
 
-function playRound() {
-    userChoice = humanChoice();
+function playRound(e) {
+    userChoice = humanChoice(e);
+    console.log(userChoice);
     compChoice = computerChoice();
+    console.log(compChoice);
     if (userChoice === "rock" && compChoice === "scissors" ||
         userChoice === "scissors" && compChoice === "paper" ||
         userChoice === "paper" && compChoice === "rock") {
-        console.log(`Computer chose ${compChoice}. You Win! `);
+        para.textContent = `Computer chose ${compChoice}.You chose ${userChoice}. You Win! `;
         userScore++;
     }
     else if (compChoice === "rock" && userChoice === "scissors" ||
         compChoice === "scissors" && userChoice === "paper" ||
         compChoice === "paper" && userChoice === "rock") {
-        console.log(`Computer chose ${compChoice}. You Lose! `);
+        para.textContent = `Computer chose ${compChoice}.You chose ${userChoice}. You Lose! `;
         compScore++;
     }
     else {
-        console.log(`Its draw.Comp chose ${compChoice}`);
+        para.textContent = `Its draw.Both You and Comp chose ${compChoice}`;
     }
-}
+    score.textContent = `Your points ${userScore} . Comp points ${compScore}`;
 
-function playGame() {
-    let i = 0;
-    while (i < 5) {
-        playRound();
-        i++;
+    if (userScore == 5 || compScore == 5) {
+        endGame();
     }
-    endGame();
+
 }
 
 function endGame() {
 
     if (userScore > compScore) {
-        console.log(`You won the game with ${userScore} points`);
+        para.textContent = `You won the game with ${userScore} points`;
     }
 
     else if (userScore < compScore) {
-        console.log(`Comp won the game with ${compScore} points`);
+        para.textContent = `Comp won the game with ${compScore} points`;
     }
 
     else {
-        console.log(`Game is draw with ${compScore} for both sides`);
+        para.textContent = `Game is draw with ${compScore} points for both sides`;
     }
-    console.log("Game finished!Play Again using playGame()");
+    score.textContent = "Game finished!Play Again";
     userScore = 0;
     compScore = 0;
 }
+
+const btns = document.querySelectorAll("button");
+
+
+const result = document.querySelector(".result");
+const para = document.createElement("p");
+const score = document.createElement("p");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", playRound);
+})
+
+result.appendChild(para);
+result.appendChild(score);
+
+
